c7d6923b2163b7da75a3c5226b4210a7e291a9c5
Tim Plunkett <git@plnktt.com>
2017-04-07 09:43:53 -0700

    Issue #2699085 by tim.plunkett: Lock Panels IPE when a user has made TempStore changes.

--- tests/src/FunctionalJavascript/PanelizerIntegrationTest.php
+++ tests/src/FunctionalJavascript/PanelizerIntegrationTest.php
@@ -61,4 +61,27 @@ class PanelizerIntegrationTest extends PanelsIPETestBase {
     $this->test_route = 'node/1';
   }
 
+  /**
+   * Tests that the IPE editing session is specific to a user.
+   */
+  public function testUserEditSession() {
+    $this->visitIPERoute();
+    $this->assertSession()->elementExists('css', '.layout--onecol');
+
+    // Change the layout to lock the IPE.
+    $this->changeLayout('Columns: 2', 'layout_twocol');
+    $this->assertSession()->elementExists('css', '.layout--twocol');
+    $this->assertSession()->elementNotExists('css', '.layout--onecol');
+
+    // Create a second node.
+    $this->drupalGet('node/add/page');
+    $this->submitForm(['title[0][value]' => 'Test Node 2'], t('Save and publish'));
+    $this->test_route = 'node/2';
+
+    // Ensure the second node does not use the session of the other node.
+    $this->visitIPERoute();
+    $this->assertSession()->elementExists('css', '.layout--onecol');
+    $this->assertSession()->elementNotExists('css', '.layout--twocol');
+  }
+
 }
